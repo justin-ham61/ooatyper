c91b0d65a3343cc6c006204334384422b5900200
Justin <jiheon.ham61@gmail.com>
2023-01-24 02:21:04 -0800

    added a comma

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const options = {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
 };
 
 const db = mysql.createPool(options);
