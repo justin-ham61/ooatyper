9f7eb49d38d164000a06dc9391fa2df0a4d1b2f9
Justin <jiheon.ham61@gmail.com>
2023-01-24 20:05:56 -0800

    set port to 3000

--- app.js
+++ app.js
@@ -531,7 +531,7 @@ const { waitForDebugger } = require('inspector');
 const { all } = require('./routes/users.js');
 app.use('/users', userRouter)
 
-const port = process.env.port || 3003;
+const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`server is running on localhost:${port}`);
 })
